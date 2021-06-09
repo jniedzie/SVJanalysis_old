9f2f468510dfeb22eea7771e50a008da92866a89
Florian Eble <florian.eble@cern.ch>
2021-06-09 17:30:24 +0200

    underscore variables

--- plottingTools/nPanelsPlots/mergePDFs.sh
+++ plottingTools/nPanelsPlots/mergePDFs.sh
@@ -2,10 +2,10 @@
 
 ##################################    README   #################################
 #
-# Merge PDFs in ${figuresDir} into ${mergedPDF}.
-# PDFs can be merged in alphabetic order, order="alpha", or by decreasing auc,
+# Merge pdfs in ${figures_dir} into ${merged_pdf}.
+# pdfs can be merged in alphabetic order, order="alpha", or by decreasing auc,
 # order="auc".
-# If order="auc", then the path to the auc csv file, ${aucFile}, must be
+# If order="auc", then the path to the auc csv file, ${auc_file}, must be
 # provided. The auc file must have the following form:
 #   variable, auc
 #   MET_pt,   0.95
@@ -16,38 +16,38 @@
 ################################################################################
 
 
-figuresDir=./figures/
-mergedPDF=merged.pdf
+figures_dir=./figures/
+merged_pdf=merged.pdf
 order="auc"  # choices: "alpha", "auc"
-aucFile=auc.csv
+auc_file=auc.csv
 
-## Make list of PDFs...
-PDFs=""
+## Make list of pdfs...
+pdfs=""
 
 # ... in alphabetic order
 if [ "${order}" == "alpha" ]; then
-  for file in `ls ${figuresDir}`; do
+  for file in `ls ${figures_dir}`; do
     a=${#file}-4
     if [ "${file:a:4}" = ".pdf" ]; then
-      PDFs=${PDFs}' '${figuresDir}${file}
+      pdfs=${pdfs}' '${figures_dir}${file}
     fi
   done
 
 # ... in decreasing auc order
 elif [ "${order}" == "auc" ]; then
-  n=$(($(cat ${aucFile} | wc -l)-1))
-  variables=`cat ${aucFile} | cut -d, -f1 | tail -n ${n}`
+  n=$(($(cat ${auc_file} | wc -l)-1))
+  variables=`cat ${auc_file} | cut -d, -f1 | tail -n ${n}`
   for variable in ${variables[@]}; do
-    PDFs=${PDFs}' '${figuresDir}${variable}.pdf
+    pdfs=${pdfs}' '${figures_dir}${variable}.pdf
   done
 fi
 
 ## Merge PDF
-gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=${mergedPDF} -dBATCH ${PDFs} > /dev/null 2>&1
+gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=${merged_pdf} -dBATCH ${pdfs} > /dev/null 2>&1
 
 ## Print out if merging was successful
 if [ "$?" = "0" ]; then
-  echo "${mergedPDF} has been created"
+  echo "${merged_pdf} has been created"
 else
-  echo "Could not create ${mergedPDF}"
+  echo "Could not create ${merged_pdf}"
 fi
