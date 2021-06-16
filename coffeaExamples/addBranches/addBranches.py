238539c8f31b3b3b580d24414a9ac9ea436ae5cf
Florian Eble <florian.eble@cern.ch>
2021-06-16 14:10:25 +0200

    Example for adding ptD to a tree

---
+++ coffeaExamples/addBranches/addBranches.py
@@ -0,0 +1,202 @@
+import coffea as cf
+from coffea import processor
+from coffea.nanoevents import BaseSchema, NanoAODSchema, PFNanoAODSchema
+#from coffea.nanoevents import BaseSchema, NanoAODSchema
+import awkward as ak
+import awkward0 as ak0
+import uproot
+import uproot3
+import numpy as np
+import time
+import argparse
+import os
+import sys
+import json
+
+sys.path.append("../pythonUtils/")
+import utilities as utl
+import processorAddBranches
+
+
+def make_input_files_list(input_files):
+
+    # If coma separated list of ROOT files (which ends with the .root of the last ROOT file)
+    if input_files.endswith(".root"):
+        input_files = input_files.split(",")
+    # Else list of ROOT files in txt file
+    else:
+        with open (input_files, "r") as txt_file:
+            input_files = txt_file.readlines()
+        input_files = [ x.replace("\n", "") for x in input_files ]
+        input_files = [ x for x in input_files if x.endswith(".root") ]
+
+    return input_files
+
+
+def make_events_branches(accumulator, debug):
+    """Make branches for Events tree."""
+
+    branches = {}
+    branches_init = {}
+    len_keys = []
+
+    # Finding keys giving the length of jagged arrays
+    for k, v in accumulator.items():
+        nkey = "n"+str(k.split("_")[0])
+        if (not k.startswith("n")) and (nkey in accumulator.keys()):
+            len_keys.append(nkey)
+
+    # Making branches
+    # Need to use ak0 because it is not yet implemented in uproot4 i.e. ak1 (Feb. 2021)
+    for k, v in accumulator.items():
+        if debug:
+            print("%s: %s" %(k, ak.type(ak.Array(v.value))))
+        if not k.startswith("n"):
+            nkey = "n"+str(k.split("_")[0])
+            if nkey in len_keys:
+                branches[k] = ak.to_awkward0(ak.Array(v.value))
+                # Case distinction for type of the jagged-array collections, treated as "object" in the processor
+                # _candIdx and _jetIdx must be saved as integers ("i4") to use array at once syntax like 
+                #     jetIdx = (events.JetPFCandsAK4_jetIdx == ijet)
+                #     candIdx = events.JetPFCandsAK4_candIdx[jetIdx]
+                #     PFcands_eta = events.JetPFCands_eta[candIdx]
+                if k.endswith("_candIdx") or k.endswith("_jetIdx"):
+                    branches_init[k] = uproot3.newbranch(np.dtype("i4"), size=nkey)
+                else:
+                    branches_init[k] = uproot3.newbranch(np.dtype("f8"), size=nkey)
+            else:
+                branches[k] = ak.to_awkward0(ak.Array(v.value))
+                branches_init[k] = uproot3.newbranch(v.value.dtype)
+        else:
+            if k in len_keys:
+                branches[k] = ak.to_awkward0(ak.Array(v.value))
+            else:
+                branches[k] = ak.to_awkward0(ak.Array(v.value))
+                branches_init[k] = uproot3.newbranch(v.value.dtype)
+
+    return branches, branches_init
+
+
+def write_root_file(accumulator, output_file, debug):
+    """
+    """
+
+    output_file = "test.root" # For test purposes
+
+    # Making branches to write to Events tree
+    branches, branches_init = make_events_branches(accumulator, debug)
+
+    if debug:
+        print("\nEvents branches keys:")
+        print(branches.keys())
+        print("\nEvents branches_init keys:")
+        print(branches_init.keys())
+
+
+    # Save branches to ROOT file
+    # Need to use uproot3 because it is not implemented yet in uproot4 (Feb. 2021)
+    with uproot3.recreate(output_file) as f:
+        f["Events"] = uproot3.newtree(branches_init)
+        f["Events"].extend(branches)
+        print("\nTTree Events saved to output file %s" %output_file)
+
+    return
+
+
+def main(input_file, processor_name, schema, pf_nano_aod_version, chunksize, maxchunks, nworkers, debug):
+
+    print("Input file:")
+    print(input_file)
+
+    ## Fileset
+    fileset = { "fileset": [input_file] }
+
+    print(processorAddBranches)
+
+    ## Make pre-selections
+    output = processor.run_uproot_job(
+        fileset,
+        treename = "Events",
+        processor_instance = getattr(processorAddBranches, processor_name)(schema, pf_nano_aod_version),
+        executor = processor.iterative_executor,
+        executor_args = {"schema": eval(schema+"Schema"), "workers": nworkers},
+        chunksize = chunksize,
+        maxchunks = maxchunks,
+        )
+
+    ## Making output ROOT file
+    # if output not empty:
+    write_root_file(output, input_file, debug)
+
+
+
+if __name__ == "__main__":
+
+    tstart = time.time()
+
+    ## Parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i", "--inputfiles",
+        help="Input ROOT files to skim. Format: Comma separated list of files "\
+             +"or txt file with list of file (1 file name per line)",
+        # Example for the format:
+        # Ex1: file1.root,file2.root
+        # Ex2: $ cat input.txt
+        #      file1.root
+        #      file2.root
+        required=True
+        )
+    parser.add_argument(
+        "-p", "--processor",
+        help="Coffea processor to be used, as defined in processor.py",
+        required=True
+        )
+    parser.add_argument(
+        "-s", "--schema",
+        help="Coffea nanoevents schema",
+        choices=["Base", "PFNanoAOD"],
+        required=True
+        )
+    parser.add_argument(
+        "-pv", "--pfnanoaodversion",
+        help="PF nano AOD version",
+        choices=["102X", "106X"],
+        required=True
+        )
+    parser.add_argument(
+        "-c", "--chunksize",
+        help="Size of the data chunks (default=100000)",
+        default=100000, type=int
+        )
+    parser.add_argument(
+        "-m", "--maxchunks",
+        help="Maximum number of chunks to process, no flag means no maximum",
+        type=int
+        )
+    parser.add_argument(
+        "-n", "--nworkers",
+        help="Number of worker nodes (default=4)",
+        default=4, type=int
+        )
+    parser.add_argument(
+        "-debug", "--debug",
+        help="Debug mode",
+        action="store_true",
+        )
+    
+    
+    args = parser.parse_args()
+
+    ## Get list of input ROOT files
+    input_files = make_input_files_list(args.inputfiles)
+
+    ## Make pre-selection
+    for input_file in input_files:
+        main(input_file, args.processor, args.schema, args.pfnanoaodversion, args.chunksize, args.maxchunks, args.nworkers, args.debug)
+
+
+    elapsed = time.time() - tstart
+    print("\nTotal elapsed time: %d s" %elapsed)
+
+
---
+++ coffeaExamples/addBranches/processorAddBranches.py
@@ -0,0 +1,137 @@
+from coffea import processor
+import awkward as ak
+import numpy as np
+import sys
+
+
+sys.path.append("../pythonUtils/")
+import awkwardArrayUtils as akutl
+
+
+## Define some short-hands for column and value accumulator
+def column_accumulator(type_):
+    return processor.column_accumulator(np.array([], dtype=type_))
+
+
+def calculate_ptD(pf_cands):
+    """Calculate ptD for each jet in all events.
+
+    Calculate ptD for each jet in all events using optimized columnar
+    operations. However, cannot avoid copy of the array at the end to
+    transpose / swap axes of the ak array.
+
+    Args:
+        pf_cands (awkward.Array):
+            Ak array where axis 0 is events axis and axis 1 has pt and jetIdx
+            fields.
+    
+    Returns:
+         awkward.Array
+
+    Examples:
+        >>> pf_cands_idx = ak.Array([[0,0,1,1,1,2,2,2], [0,0,1,2,2,3]])
+        >>> jet_cands_pt = ak.Array([[180,150,150,130,70,180,100,20], [500, 300, 400, 300, 250, 100]])
+        >>> pf_cands = ak.zip({"jetIdx": jet_cands_idx, "pt": jet_cands_pt})
+        >>> calculate_ptD(pf_cands)
+        [[0.71, 0.601, 0.69], [0.729, 1, 0.71, 1]]
+    """
+
+    max_idx = ak.max(pf_cands.jetIdx)
+
+    for jet_idx in range(max_idx+1):
+        pf_cands_pt = pf_cands[(pf_cands.jetIdx == jet_idx)].pt
+
+        ptD_denominator = ak.sum(pf_cands_pt, axis=1)
+        ptD_numerator = np.sqrt(ak.sum(pf_cands_pt**2, axis=1))
+        ptD_tmp = akutl.divide_ak_arrays(ptD_numerator, ptD_denominator, division_by_zero_value=-1)
+
+        if jet_idx == 0:
+            ptD = [ptD_tmp]
+        else:
+            ptD = ak.concatenate((ptD, [ptD_tmp]), axis=0)
+
+    # Need to transpose / swap axes of the ak array to have events in the first axis
+    # instead of jets. Maybe there is something smarter than that.
+    np_array = ak.to_numpy(ptD).T
+    ak_array = ak.Array([np_array])     # Can't avoid that copy in memory!!!
+    ptD = ak_array[ak_array != -1][0]
+
+    return ptD
+
+
+def get_pf_cands(events, schema, pf_nano_aod_version):
+
+    if schema == "Base":
+        if pf_nano_aod_version == "102X":
+            pf_cands = ak.zip({
+                "jetIdx": events.FatJetPFCands_jetIdx,
+                "pt": events.FatJetPFCands_pt,
+            })
+        if pf_nano_aod_version == "106X":
+            pf_cands = ak.zip({
+                "jetIdx": events.JetPFCandsAK8_jetIdx,
+                "pt": events.JetPFCands_pt[events.JetPFCandsAK8_candIdx]
+            })
+    elif schema == "PFNanoAOD":
+        if pf_nano_aod_version == "102X":
+            pf_cands = ak.zip({
+                "jetIdx": events.FatJetPFCands.jetIdx,
+                "pt": events.FatJetPFCands.pt,
+            })
+        if pf_nano_aod_version == "106X":
+            pf_cands = ak.zip({
+                "jetIdx": events.JetPFCandsAK8.jetIdx,
+                "pt": events.JetPFCands[events.JetPFCandsAK8.candIdx].pt,
+            })
+
+    return pf_cands
+
+
+def get_n_fat_jet(events, schema):
+    if schema == "Base":
+        return events.nFatJet
+    elif schema == "PFNanoAOD":
+        return ak.count(events.FatJet.pt, axis=1)
+
+
+class BranchesMaker(processor.ProcessorABC):
+    """Example for calculating ptD."""
+
+
+    def __init__(self, schema, pf_nano_aod_version):
+
+        self.schema = schema
+        self.pf_nano_aod_version = pf_nano_aod_version
+
+        branches = { 
+            "FatJet_ptD"      : column_accumulator(np.float64),
+            "nFatJet"         : column_accumulator(np.float64),
+        }
+ 
+        ## Define accumulator
+        self._accumulator = processor.dict_accumulator({
+            **branches
+            })
+
+
+    @property
+    def accumulator(self):
+        return self._accumulator
+
+
+    def process(self, events):
+        """Compute new quantities to be added to the NTuple."""
+
+        ## Define accumulator
+        output = self.accumulator.identity()
+
+        pf_cands = get_pf_cands(events, self.schema, self.pf_nano_aod_version)
+        output["FatJet_ptD"] = processor.column_accumulator(np.array(ak.to_list(calculate_ptD(pf_cands)), dtype=object))
+        output["nFatJet"] = processor.column_accumulator(ak.to_numpy(get_n_fat_jet(events, self.schema)))
+
+        return output
+
+
+    def postprocess(self, accumulator):
+        return accumulator
+
