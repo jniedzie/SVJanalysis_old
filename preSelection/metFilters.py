01a07bc5771856fcd9a6c7ebc179619a61812907
Florian Eble <florian.eble@cern.ch>
2021-09-06 15:17:04 +0200

    Add file with MET filters

---
+++ preSelection/metFilters.py
@@ -0,0 +1,11 @@
+met_filters = [
+    "goodVertices",
+    "globalSuperTightHalo2016Filter",
+    "HBHENoiseFilter",
+    "HBHENoiseIsoFilter",
+    "EcalDeadCellTriggerPrimitiveFilter",
+    "BadPFMuonFilter",
+    "eeBadScFilter",
+]
+
+
