4019d9b042f014f4f139fcdb2b052567ba4811e4
Florian Eble <florian.eble@cern.ch>
2021-09-07 11:16:36 +0200

    List files using file name expansion

--- utilities/utilities.py
+++ utilities/utilities.py
@@ -1,6 +1,20 @@
 from collections import Counter
 import re
 import json
+import subprocess
+
+
+def run_bash_command(bash_command):
+    """Returns the output of a bash command.
+
+    Args:
+        bash_command (str)
+
+    Returns:
+        str
+    """
+
+    return subprocess.Popen(bash_command, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")[:-1]
 
 
 def make_file_list(files_arg):
@@ -15,8 +29,12 @@ def make_file_list(files_arg):
         list[str]
     """
 
+    # If the file argument has file name expansion (not really a regex, ls does not support regex)
+    if "*" in files_arg or "!" in files_arg or "?" in files_arg or "^" in files_arg \
+    or "[" in files_arg or "{" in files_arg:
+        root_files = run_bash_command("ls %s 2>/dev/null" %files_arg).split("\n")
     # If list of ROOT files in txt file
-    if not files_arg.endswith(".root"):
+    elif not files_arg.endswith(".root"):
         with open (files_arg, "r") as txt_file:
             root_files = [ x.replace("\n", "") for x in txt_file.readlines() ]
     # Else we assume coma separated list of ROOT files
