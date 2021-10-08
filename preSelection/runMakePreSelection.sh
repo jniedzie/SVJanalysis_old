f8939294ee964ed1e4d963b6da835b59dc342b1d
Florian Eble <florian.eble@cern.ch>
2021-10-08 15:46:17 +0200

    Update example command line

--- preSelection/runMakePreSelection.sh
+++ preSelection/runMakePreSelection.sh
@@ -2,5 +2,4 @@
 
 input_file=/pnfs/psi.ch/cms/trivcat/store/user/fleble/SVJ/t_channel/samples/2018/PFNANOAOD/step5_PFNANOAOD_t-channel_mMed-3000_mDark-20_rinv-0.3_alpha-peak_yukawa-1_13TeV-madgraphMLM-pythia8_n-1000_part-1.root
 
-python makePreSelection.py -i ${input_file} -o test.root -t PFNanoAOD_106X_v02 -p Preselection_tchannel
-
+python makePreSelection.py -i ${input_file} -o test.root -t PFNanoAOD_106X_v02 -c 100 -m 4 -p Preselection_tchannel -xsec 0.01891
